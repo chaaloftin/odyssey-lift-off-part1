4a4cfad2bc7f362ae25020c9f9632eaac6526051
Chaa Loftin <chaamail@yahoo.com>
2022-03-01 16:31:46 -0500

    Defined boilerplate code for server

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,14 @@
-//TODO
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+
+// make server defined by schema
+const server = new ApolloServer({ typeDefs });
+
+// instantiate server
+server.listen().then(() => {
+  console.log(`
+        Server is running!
+        Listening on port 4000
+        Query at https://studio.apollographql.com/dev
+    `);
+});
